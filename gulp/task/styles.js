5242cb1af30ca722a5830ad4b56dd1ac5e8254f9
Ediur <daniart83@gmail.com>
2018-03-22 20:31:38 +0100

    styled up to before features

--- gulp/task/styles.js
+++ gulp/task/styles.js
@@ -4,10 +4,11 @@ const autoprefixer = require('autoprefixer');
 const cssvars = require('postcss-simple-vars');
 const nested = require('postcss-nested');
 const cssImport = require('postcss-import');
+const mixins = require('postcss-mixins');
 
 gulp.task("styles", () => {
     return gulp.src('./app/assets/styles/style.css')
-        .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))        
+        .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))        
         .pipe(gulp.dest('./app/temp/styles'));
 });
 
