6dbb130d53acaa65c01e686448784d22e7b312f3
Ediur <daniart83@gmail.com>
2018-03-25 16:09:39 +0200

    Completed gulp-sprite branch, ready to merge with master

--- gulp/task/styles.js
+++ gulp/task/styles.js
@@ -1,14 +1,17 @@
-const gulp = require('gulp');
-const postcss = require('gulp-postcss');
-const autoprefixer = require('autoprefixer');
-const cssvars = require('postcss-simple-vars');
-const nested = require('postcss-nested');
-const cssImport = require('postcss-import');
-const mixins = require('postcss-mixins');
+var gulp = require('gulp'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested'),
+cssImport = require('postcss-import'),
+mixins = require('postcss-mixins');
 
-gulp.task("styles", () => {
-    return gulp.src('./app/assets/styles/style.css')
-        .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))        
-        .pipe(gulp.dest('./app/temp/styles'));
+gulp.task('styles', function() {
+  return gulp.src('./app/assets/styles/style.css')
+    .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
+    // .on('error', function(errorInfo) {
+    //   console.log(errorInfo.toString());
+    //   this.emit('end');
+    // })
+    .pipe(gulp.dest('./app/temp/styles'));
 });
-
--- gulp/task/watch.js
+++ gulp/task/watch.js
@@ -1,6 +1,28 @@
-const gulp = require('gulp');
-const watch = require('gulp-watch');
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+browserSync = require('browser-sync').create();
 
-gulp.task('watch', () => {    
-    watch('./app/assets/styles/**/*.css', () => gulp.start('styles'));
+gulp.task('cssInject', ['styles'], function() {
+  return gulp.src('./app/temp/styles/style.css')
+    .pipe(browserSync.stream());
 });
+
+gulp.task('watch', function() {
+
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: "app"
+    }
+  });
+
+  watch('./app/index.html', function() {
+    browserSync.reload();
+  });
+
+  watch('./app/assets/styles/**/*.css', function() {
+    gulp.start('cssInject');
+  });
+
+});
+
